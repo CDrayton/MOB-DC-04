0f09b47f7567c18b6634ddc58ad8ce3cee14b665
CDrayton <Caeden_Drayton@yahoo.com>
2015-11-23 19:12:02 -0500

    From last week 11/19

---
+++ Classwork/Class 14/Students/Students/Students.swift
@@ -0,0 +1,18 @@
+//
+//  Students.swift
+//  Students
+//
+//  Created by newww on 11/16/15.
+//  Copyright © 2015 Thomas Degry. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Students: NSManagedObject { //Next to last object in tier pyramid
+    @NSManaged var name: String
+    @NSManaged var location: String
+    
+    
+}
+
